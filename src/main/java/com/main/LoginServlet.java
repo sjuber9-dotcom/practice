32dcad2c4d7066a1317dd86b7dd20ae208a9cd8e
sjuber9-dotcom <sjuber9@gmail.com>
2026-03-29 18:28:00 +0530

    first commit

---
+++ src/main/java/com/main/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String uname = req.getParameter("uname");
+        String pass = req.getParameter("pass");
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        if (uname.equals("admin") && pass.equals("admin")) {
+            out.println("<h2>Login Successful</h2>");
+        } else {
+            out.println("<h2>Invalid Credentials</h2>");
+        }
+    }
+}
+
---
+++ src/main/java/com/main/RegisterStudentServlet.java
@@ -0,0 +1,25 @@
+package com.main;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/registerStudent")
+public class RegisterStudentServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String name = req.getParameter("sname");
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        out.println("<h2>Student Registered Successfully</h2>");
+        out.println("<h3>Welcome " + name + "</h3>");
+        out.println("<a href='login.html'>Back to Login</a>");
+    }
+}
